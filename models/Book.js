f37ba9c46ed0a98ef61bfd5d08e60f1400239101
haywah27 <hayleywahlroos15@gmail.com>
2021-05-02 14:43:49 -0700

    creating schema to save books

--- models/Book.js
+++ models/Book.js
@@ -1,2 +1,15 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
+const BookSchema = new Schema({
+    id: { type: String, required: true},
+    title: { type: String },
+    author: { type: Array },
+    image: { type: String },
+    description: { type: String },
+    link: { type: String },
+});
+
+const Books = mongoose.model("Books", BookSchema);
+
+module.exports = Books;
