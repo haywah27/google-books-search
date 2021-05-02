a9510e7a1555550d1f3fb06081d4bc06e78e7e1c
haywah27 <hayleywahlroos15@gmail.com>
2021-05-02 16:11:37 -0700

    reintegrating new file structure

---
+++ client/src/pages/Search/SearchBook.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Navbar, Nav } from "react-bootstrap";
+
+function SearchBook() {
+  return (
+    <>
+    hello from search page
+    </>
+  );
+}
+
+export default SearchBook;
