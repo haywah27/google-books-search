d8c865dd27a3dd7566b6b3eb91ed582cdd3d7d13
haywah27 <hayleywahlroos15@gmail.com>
2021-05-02 15:26:29 -0700

    setting up navigation, saved and search files

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React from "react"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navigation from "../src/components/Nav/Nav"
+import SearchBook from "./pages/Search/Search"
+import SavedBooks from "../src/pages/Saved/Save"
 import "./App.css";
 
 function App() {
@@ -9,7 +12,7 @@ function App() {
       <Navigation />
       <Switch>
           <Route exact path="/" component={SearchBook} />
-          <Route exact path="/library" component={SavedBooks} />
+          <Route exact path="/saved-books" component={SavedBooks} />
         </Switch>
     </Router>
     </>
