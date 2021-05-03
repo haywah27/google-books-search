313b179a6811fb94f221fc8f2ff6f1a550197873
haywah27 <hayleywahlroos15@gmail.com>
2021-05-02 21:21:10 -0700

    run build for deployment

---
+++ client/build/precache-manifest.d914fa093a5b9d0087706685447cb639.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d9d663a35c983d0bc9c0dc4625f5b64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d20e7642d3b69cf418ab",
+    "url": "/static/css/main.0edcbd2d.chunk.css"
+  },
+  {
+    "revision": "195911ebf631e8ebae50",
+    "url": "/static/js/2.be8fd295.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.be8fd295.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d20e7642d3b69cf418ab",
+    "url": "/static/js/main.1ad9175c.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  },
+  {
+    "revision": "dda2080cc529c9ee7773b940c536279e",
+    "url": "/static/media/background.dda2080c.jpg"
+  }
+]);
